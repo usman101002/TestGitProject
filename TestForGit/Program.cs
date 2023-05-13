e5f026279591faba2a4d0cdea4a969f806d8aa8d
Усман <@DESKTOP-E07M5P8>
2023-05-13 15:30:44 +0700

    Add project files.

---
+++ TestForGit/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace lessons
+{
+
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+        }
+
+    }
+}
+
